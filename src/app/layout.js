090b50117bfa20edfbb5b2ffd891b7b7d2e81c8f
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-07-25 17:48:30 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +10,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <header>
+          <h1>This is Header</h1>
+        </header>
+        {children}
+        <footer>
+          <p>This is Footer</p>
+        </footer>
+      </body>
     </html>
   )
 }
